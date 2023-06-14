5efa9dd9e2f23fa06c7811849f44712adb3acb5c
nandan-varma <nandanvarma.me@outlook.in>
2023-06-14 21:35:23 +0530

    removed blogs option

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import Particles from "./components/particles";
 
 const navigation = [
-	{ name : "Blog", href: "https://blog.nandanvarma.com" },
+	// { name : "Blog", href: "https://blog.nandanvarma.com" },
 	{ name: "Projects", href: "/projects" },
 	{ name: "Contact", href: "/contact" },
 ];
