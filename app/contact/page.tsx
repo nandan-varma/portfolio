c5dd269d04e616b1db4fd452ce6c0081bb945bcc
nandan-varma <nandanvarma.me@gmail.com>
2024-02-02 19:43:48 +0530

    updated home page with bad a11y (sorry)

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -12,25 +12,19 @@ const socials = [
 	// 	handle: "@Nandan Varma_",
 	// },
 	{
-		icon: <Mail size={20} />,
-		href: "mailto:nandanvarma@icloud.com",
+		icon: <Mail size={32} />,
+		href: "mailto:nandanvarma.me@gmail.com",
 		label: "Email",
 		handle: "",
 	},
 	{
-		icon: <Phone size={20} />,
-		href: "tel:8639846030",
-		label: "Phone",
-		handle: "",
-	},
-	{
-		icon: <Github size={20} />,
+		icon: <Github size={32} />,
 		href: "https://github.com/nandan-varma",
 		label: "Github",
 		handle: "",
 	},
 	{
-		icon: <Linkedin size={20}/>,
+		icon: <Linkedin size={32}/>,
 		href: "https://www.linkedin.com/in/nandanvarma/",
 		label: "LinkedIn",
 		handle: "",
@@ -42,7 +36,7 @@ export default function Example() {
 		<div className=" bg-gradient-to-tl from-zinc-900/0 via-zinc-900 to-zinc-900/0">
 			<Navigation />
 			<div className="container flex items-center justify-center min-h-screen px-4 mx-auto">
-				<div className="grid w-full grid-cols-1 gap-8 mx-auto mt-32 sm:mt-0 sm:grid-cols-4 lg:gap-16">
+				<div className="grid w-full grid-cols-1 gap-6 mx-auto mt-32 sm:mt-0 sm:grid-cols-3 lg:gap-16">
 					{socials.map((s) => (
 						<Card>
 							<Link
@@ -51,10 +45,10 @@ export default function Example() {
 								className="p-4 relative flex flex-col items-center gap-4 duration-700 group md:gap-8 md:py-24  lg:pb-48  md:p-16"
 							>
 								<span
-									className="absolute w-px h-2/3 bg-gradient-to-b from-zinc-500 via-zinc-500/50 to-transparent"
+									className="absolute w-px h-2/3"
 									aria-hidden="true"
 								/>
-								<span className="relative z-10 flex items-center justify-center w-12 h-12 text-sm duration-1000 border rounded-full text-zinc-200 group-hover:text-white group-hover:bg-zinc-900 border-zinc-500 bg-zinc-900 group-hover:border-zinc-200 drop-shadow-orange">
+								<span className="relative z-10 flex items-center justify-center w-20 h-20 text-sm duration-1000 border rounded-full text-zinc-200 group-hover:text-white group-hover:bg-zinc-900 border-zinc-500 bg-zinc-900 group-hover:border-zinc-200 drop-shadow-orange">
 									{s.icon}
 								</span>{" "}
 								<div className="z-10 flex flex-col items-center">
--- app/page.tsx
+++ app/page.tsx
@@ -1,63 +1,77 @@
-import Link from "next/link";
-import React from "react";
-import Particles from "./components/particles";
+import Link from 'next/link';
+import SpringMotion from './components/animation';
 
 const navigation = [
-	// { name : "Blog", href: "https://blog.nandanvarma.com" },
-	{ name: "Projects", href: "/projects" },
-	{ name: "Contact", href: "/contact" },
+  { name: 'Projects', href: '/projects' },
+  { name: 'Contact', href: '/contact' },
 ];
 
-export default function Home() {
-	return (
-		<div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black">
-			<nav className="my-16 animate-fade-in">
-				<ul className="flex items-center justify-center gap-4">
-					{navigation.map((item) => (
-						<Link
-							key={item.href}
-							href={item.href}
-							className="text-sm duration-500 text-zinc-500 hover:text-zinc-300"
-						>
-							{item.name}
-						</Link>
-					))}
-				</ul>
-			</nav>
-			<div className="hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
-			<Particles
-				className="absolute inset-0 -z-10 animate-fade-in"
-				quantity={100}
-			/>
-			<h1 className="z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
-				Nandan Varma
-			</h1>
+const IndexPage = () => {
+  return (
+    <div className="flex flex-col items-center justify-center font-bold w-screen h-screen overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black">
+      <SpringMotion
+        initial={{ y: -1000 }}
+        animate={{ y: 0 }}
+        transition={{
+          type: 'spring',
+          stiffness: 120,
+          damping: 15,
+          delay: 0.5,
+        }}
+        whileHover={{ scale: 1.2 }}
+        whileTap={{ scale: 0.9 }}
+        className="text-white"
+      >
+        <nav className="my-16">
+          <ul className="flex items-center justify-center gap-4">
+            {navigation.map((item) => (
+              <Link
+                key={item.href}
+                href={item.href}
+                className="text-sm duration-500 text-zinc-500 hover:text-zinc-300"
+              >
+                {item.name}
+              </Link>
+            ))}
+          </ul>
+        </nav>
+      </SpringMotion>
 
-			<div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
-			<div className="my-16 text-center animate-fade-in">
-				<h2 className="text-sm text-zinc-500 ">
-					Software Developer
-					{/* {": )"} */}
-					{/* <Link
-						target="_blank"
-						href="/projects"
-						className="underline duration-500 hover:text-zinc-300"
-					>
-						Upstash
-					</Link>
+      <SpringMotion
+        initial={{ x: -1000 }}
+        animate={{ x: 0 }}
+        transition={{
+          type: 'spring',
+          stiffness: 120,
+          damping: 15,
+          delay: 0.1,
+        }}
+        whileHover={{ scale: 1.2 }}
+        whileTap={{ scale: 0.9 }}
+        className="font-bold text-white"
+      >
+        <h1 className="z-10 text-4xl cursor-default font-display sm:text-6xl ">Nandan Varma</h1>
+      </SpringMotion>
 
-					<br />
-					and working on{" "}
-					<Link
-						target="_blank"
-						href="/projects"
-						className="underline duration-500 hover:text-zinc-300"
-					>
-						planetfall.io
-					</Link>{" "}
-					at night. */}
-				</h2>
-			</div>
-		</div>
-	);
-}
+      <SpringMotion
+        initial={{ y: 1000 }}
+        animate={{ y: 0 }}
+        transition={{
+          type: 'spring',
+          stiffness: 120,
+          damping: 15,
+          delay: 0.3,
+        }}
+        whileTap={{ scale: 0.9 }}
+		whileHover={{ scale: 1 }}
+        className="text-white"
+      >
+        <div className="my-16 text-center">
+          <h2 className="text-sm text-zinc-500 ">Software Developer</h2>
+        </div>
+      </SpringMotion>
+    </div>
+  );
+};
+
+export default IndexPage;
