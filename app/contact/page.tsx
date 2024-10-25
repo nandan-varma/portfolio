871078b04d20af776880f2953a811186b5d1e085
Nandan Varma <74198723+nandan-varma@users.noreply.github.com>
2024-10-25 05:31:37 +0000

    About page updated to show handles

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,4 +1,3 @@
-"use client";
 import { Phone, Github, Mail, Twitter , Linkedin } from "lucide-react";
 import Link from "next/link";
 import { Navigation } from "../components/nav";
@@ -18,21 +17,21 @@ const socials = [
 	// },
 	{
 		icon: <Mail size={32} />,
-		href: "mailto:nandanvarma.me@gmail.com",
+		href: "mailto:contact@nandanvarma.com",
 		label: "Email",
-		handle: "",
+		handle: "contact@nandanvarma.com",
 	},
 	{
 		icon: <Github size={32} />,
 		href: "https://github.com/nandan-varma",
 		label: "Github",
-		handle: "",
+		handle: "nandan-varma",
 	},
 	{
 		icon: <Linkedin size={32}/>,
 		href: "https://www.linkedin.com/in/nandanvarma/",
 		label: "LinkedIn",
-		handle: "",
+		handle: "nandanvarma",
 	}
 ];
 
@@ -57,7 +56,7 @@ export default function Example() {
 									{s.icon}
 								</span>{" "}
 								<div className="z-10 flex flex-col items-center">
-									<span className="text-xl font-medium duration-150 lg:text-3xl text-zinc-200 group-hover:text-white font-display">
+									<span className="text-xl font-medium duration-150 lg:text-xl text-zinc-200 group-hover:text-white font-display">
 										{s.handle}
 									</span>
 									<span className="mt-4 text-sm text-center duration-1000 text-zinc-400 group-hover:text-zinc-200">
