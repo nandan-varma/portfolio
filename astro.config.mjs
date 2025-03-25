876e5618995797324ef6cee565af250fb9bf3600
Nandan Varma <74198723+nandan-varma@users.noreply.github.com>
2025-03-25 23:38:44 +0000

    UI fixes and removed react

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,12 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
-import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.nandanvarma.com',
-  integrations: [mdx(), sitemap(), react()],
+  integrations: [mdx(), sitemap()],
 
   vite: {
     plugins: [tailwindcss()],
