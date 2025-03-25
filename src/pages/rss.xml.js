35e779128158e3ae208146cbc739ff6ace248903
Nandan Varma <74198723+nandan-varma@users.noreply.github.com>
2025-03-25 23:38:44 +0000

    UI fixes and removed react

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -10,7 +10,7 @@ export async function GET(context) {
 		site: context.site,
 		items: posts.map((post) => ({
 			...post.data,
-			link: `/project/${post.id}/`,
+			link: `/projects/${post.id}/`,
 		})),
 	});
 }
