fbb98d486a2ee51a1d0afa53c8793fd967822a9a
nandan-varma <nandanvarma.me@gmail.com>
2026-04-08 19:02:37 -0700

    Added /resume endpoint

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,30 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import tailwindcss from '@tailwindcss/vite';
-import vercel from '@astrojs/vercel';
-import react from '@astrojs/react';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import tailwindcss from "@tailwindcss/vite";
+import vercel from "@astrojs/vercel";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.nandanvarma.com',
+  site: "https://www.nandanvarma.com",
   integrations: [mdx(), sitemap(), react()],
 
   vite: {
     plugins: [tailwindcss()],
     build: {
       rollupOptions: {
-        external: ['pyodide'],
+        external: ["pyodide"],
       },
-    }
+    },
   },
 
   redirects: {
-    '/cv': 'https://qujr12qsco.ufs.sh/f/fthLDAMTNUTlwdChqLZue7hJ43mSDHQtXwVdx90vZ8kIKgM5',
+    "/cv":
+      "https://qujr12qsco.ufs.sh/f/fthLDAMTNUTlwdChqLZue7hJ43mSDHQtXwVdx90vZ8kIKgM5",
+    "/resume":
+      "https://qujr12qsco.ufs.sh/f/fthLDAMTNUTl5rrcbpwWRhT2tVkEcKydfjnYDi6NpovL79sZ",
   },
 
   adapter: vercel(),
