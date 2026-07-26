aaa03632d43ff38fc917aedcc59a8b6624bfff9c
Nandan Varma <nandanvarma.me@gmail.com>
2026-07-26 10:50:57 -0700

    new project and better SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://www.nandanvarma.com",
+  site: "https://www.nandan.fyi",
   integrations: [mdx(), sitemap()],
 
   vite: {
